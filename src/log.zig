9371a562866a757f13062ff00dea93d63af75aac
Roberto Calderon Juarez <rcalderonj@pm.me>
2025-12-10 17:31:09 -0600

    Add clock support to logging

--- src/log.zig
+++ src/log.zig
@@ -1,12 +1,17 @@
 const std = @import("std");
 const root = @import("root.zig");
+const dev = root.dev;
 
 var DEFAULT_WRITTER: *std.Io.Writer = undefined;
+var DEFAULT_CLOCK: ?dev.clock.Clock = null;
 
 pub fn init_logging(logger: *std.Io.Writer) void {
     DEFAULT_WRITTER = logger;
     // DEFAULT_WRITTER.store(logger, .seq_cst);
     // @atomicStore(*std.Io.Writer, &DEFAULT_WRITTER, logger, .seq_cst);
+}
+pub fn set_default_clock(clock: dev.clock.Clock) void {
+    DEFAULT_CLOCK = clock;
 }
 
 pub fn log_fn(
@@ -22,6 +27,15 @@ pub fn log_fn(
     var w: *std.Io.Writer = DEFAULT_WRITTER;
 
     nosuspend {
+        if (DEFAULT_CLOCK) |clock| blk: {
+            var c = clock;
+            const now = c.now() catch break :blk;
+            const nanos = now.raw;
+            const micros = nanos / 1000;
+            const solo_nanos = nanos % 1000;
+
+            w.print("[{d: >9}.{d:0>3}]", .{ micros, solo_nanos }) catch break :blk;
+        }
         w.print(level_txt ++ prefix2 ++ format ++ "\n", args) catch return;
         w.flush() catch return;
     }
--- src/main.zig
+++ src/main.zig
@@ -5,9 +5,12 @@ const std = @import("std");
 pub var SERIAL_BUFFER: [128]u8 = undefined;
 
 pub fn kernel_main() !void {
-    var serial = dev.serial.Serial.default(&SERIAL_BUFFER);
-    root.log.init_logging(&serial.interface);
-    std.log.info("Iniciando kernel...", .{});
     var clock = dev.clock.Clock.mtime();
+    var serial = dev.serial.Serial.default(&SERIAL_BUFFER);
+
+    root.log.init_logging(&serial.interface);
+    root.log.set_default_clock(clock);
+
+    std.log.info("Iniciando kernel...", .{});
     std.log.info("MTIME = {d}", .{(try clock.now()).raw});
 }
