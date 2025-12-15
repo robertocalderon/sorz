3bef322ea799060409cbfed81a42c50182a850a8
Roberto Calderon Juarez <rcalderonj@pm.me>
2025-12-15 11:28:30 -0600

    Disable SATP at start and remove unsued code

--- src/root.zig
+++ src/root.zig
@@ -30,16 +30,12 @@ extern var _fw_stack_end: u8;
 pub export fn _start() linksection(".text.start") callconv(.naked) void {
     @setRuntimeSafety(false);
     asm volatile (
-        \\  csrr    t0, mhartid
-        \\  bnez    t0, ._start_other_cores
+        \\  csrw    satp, zero
         \\  addi    sp, sp, -16
         \\  sw      zero, 4(sp)
         \\  sw      zero, 0(sp)
         \\  addi    s0, sp, 16
         \\  call    _fw_entry
-        \\._start_other_cores:
-        \\  wfi
-        \\  j       ._start_other_cores
         :
         : [_SP] "{sp}" (@as(usize, @intFromPtr(&_fw_stack_end))),
           [_RA] "{ra}" (0),
