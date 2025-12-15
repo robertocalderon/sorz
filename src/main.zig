59dfbfef4a67fe92a5279254bf5d5d8682dbdffc
Roberto Calderon Juarez <rcalderonj@pm.me>
2025-12-15 11:31:37 -0600

    Remove m-mode references from main

--- src/main.zig
+++ src/main.zig
@@ -5,14 +5,13 @@ const std = @import("std");
 pub var SERIAL_BUFFER: [128]u8 = undefined;
 
 pub fn kernel_main() !void {
-    var clock = dev.clock.Clock.mtime();
+    const clock = dev.clock.Clock.mtime();
     var serial = dev.serial.Serial.default(&SERIAL_BUFFER);
 
     root.log.init_logging(&serial.interface);
     root.log.set_default_clock(clock);
 
     std.log.info("Iniciando kernel...", .{});
-    std.log.info("MTIME = {d}", .{(try clock.now()).raw});
 
     std.log.info("Iniciando reservador de memoria fisica", .{});
     root.phys_mem.init_physical_alloc();
@@ -44,39 +43,18 @@ pub fn kernel_main() !void {
     }
 
     std.log.info("Iniciando PMPs", .{});
-    root.pmp.init_pmp();
 
     std.log.info("Iniciando interrupciones", .{});
-    std.log.debug("mtvec = 0x{x:0>8}", .{asm volatile ("csrr %[ret], mtvec"
-        : [ret] "=r" (-> usize),
-    )});
     try root.interrupts.init(root.phys_mem.page_alloc());
-    std.log.debug("mtvec = 0x{x:0>8}", .{asm volatile ("csrr %[ret], mtvec"
-        : [ret] "=r" (-> usize),
-    )});
     var kernel_as = try root.virt_mem.init(alloc);
     kernel_as.activate();
-    std.log.info("Intentando saltar a modo supervisor...", .{});
-    root.KERNEL_AS = kernel_as;
-    root.privilege.lower_to_s_mode(smode_kernel_entry);
-}
 
-fn smode_kernel_entry() noreturn {
-    smode_kernel_main() catch |e| {
-        std.debug.panic("Panic on smode_kernel_main!!! err: {}", .{e});
-    };
-    root.qemu.exit(.Success);
-}
-
-fn smode_kernel_main() !void {
     const log = std.log.scoped(.SMODE);
     log.info("Desde modo supervisor!!", .{});
     log.debug("Configurando sstatus...", .{});
     const sstatus = root.registers.supervisor.SStatus.read();
     log.debug("sstatus = {any}", .{sstatus});
     sstatus.write();
-
-    const alloc = root.MEMORY_ALLOCATOR;
 
     log.debug("Iniciando PLIC...", .{});
     plic = dev.plic.PLIC.new();
@@ -85,7 +63,7 @@ fn smode_kernel_main() !void {
     dev.InterruptController.set_platform_controller(plic.get_interrupt_controller());
 
     std.log.debug("Doing real serial initialization after interrupt controller is ready", .{});
-    var serial = root.dev.serial.Serial.default(&.{});
+
     var serial_dev = serial.get_device();
     try serial_dev.init(alloc);
 
